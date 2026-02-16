718929f4f5321d471437f1c9611d9c245879ce9c
S. Kozyr <s.trump@gmail.com>
2026-02-16 11:34:12 +0200

    Added `m_editTimestamp` field to BookmarkData struct. Added serialization to/from KML.
    Field is updated on every bookmark modification.
    Updated unit-tests.
    
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- libs/map/bookmark_manager.cpp
+++ libs/map/bookmark_manager.cpp
@@ -2324,6 +2324,8 @@ void BookmarkManager::UpdateBookmark(kml::MarkId bmID, kml::BookmarkData const &
     bookmark->InvalidateRGBAColor();
     SetLastEditedBmColor(bookmark->GetColor());
   }
+
+  bookmark->SetEditTimeStamp(kml::TimestampClock::now());
 }
 
 void BookmarkManager::ChangeTrackColor(kml::TrackId trackId, dp::Color color)
