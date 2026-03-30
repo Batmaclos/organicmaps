1e67e1259bd19a3485746feef661c91d55aab49f
S. Kozyr <s.trump@gmail.com>
2026-03-30 15:41:50 +0300

    Do not override `m_modifiedTimestamp` when dealing with kml::BookmarkData and kml::TrackData
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- libs/map/bookmark_manager.cpp
+++ libs/map/bookmark_manager.cpp
@@ -2325,7 +2325,8 @@ void BookmarkManager::UpdateBookmark(kml::MarkId bmID, kml::BookmarkData const &
     SetLastEditedBmColor(bookmark->GetColor());
   }
 
-  bookmark->SetModifiedTimestamp(kml::TimestampClock::now());
+  if (bm.m_modifiedTimestamp == kml::Timestamp())
+    bookmark->SetModifiedTimestamp(kml::TimestampClock::now());
 }
 
 void BookmarkManager::ChangeTrackColor(kml::TrackId trackId, dp::Color color)
@@ -2343,7 +2344,8 @@ void BookmarkManager::UpdateTrack(kml::TrackId trackId, kml::TrackData const & t
   auto * track = GetTrackForEdit(trackId);
   track->SetData(trackData);
 
-  track->SetModifiedTimestamp(kml::TimestampClock::now());
+  if (trackData.m_modifiedTimestamp == kml::Timestamp())
+    track->SetModifiedTimestamp(kml::TimestampClock::now());
 }
 
 kml::MarkGroupId BookmarkManager::LastEditedBMCategory()
